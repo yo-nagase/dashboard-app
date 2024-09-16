3bc50320a180e7a7a1693b45940573698b3cec21
nagase.yoshimasa <nagase.yoshimasa@moneyforward.co.jp>
2024-09-16 13:46:00 +0900

    update linter settings

--- src/components/organization-form.tsx
+++ src/components/organization-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
