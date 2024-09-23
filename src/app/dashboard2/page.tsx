a137f80c66bffa8b503dcde8004d66c74d405d84
nagase.yoshimasa <nagase.yoshimasa@moneyforward.co.jp>
2024-09-23 23:41:03 +0900

    Add sample dashboard

---
+++ src/app/dashboard2/page.tsx
@@ -0,0 +1,25 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import Charts from "@/components/charts-01"
+import { DashboardPreview } from "@/components/dashboard-preview"
+import { OrganizationFormComponent } from "@/components/organization-form"
+import {
+  SidebarLayout,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+export default async function Page() {
+  const { cookies } = await import("next/headers")
+  return (
+    <SidebarLayout
+      defaultOpen={cookies().get("sidebar:state")?.value === "true"}
+    >
+      <TooltipProvider>
+        <AppSidebar />
+        <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
+          <DashboardPreview />
+        </main>
+      </TooltipProvider>
+    </SidebarLayout>
+  )
+}
