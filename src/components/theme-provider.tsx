7ff1e705561291e83f4f7ffff6055fd65c988248
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2025-01-28 11:40:55 +0900

    feat: Configure dark mode with Tailwind and next-themes

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,5 +5,14 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      attribute="class" // これが重要です
+      defaultTheme="system"
+      enableSystem
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
